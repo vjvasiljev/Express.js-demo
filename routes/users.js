fe8ee0a1277f69ce639bb60bb2715d068b8b8b29
Viaceslav <v.j.vasiljev@gmail.com>
2024-07-25 10:44:46 +0300

    Basic server

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("User list");
+});
+
+router.get("/new", (req, res) => {
+  res.send("New user form");
+});
+
+module.exports = router;
