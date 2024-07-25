cc256f40ad99d5553ce069aaf2d80ee79964c6cf
Viaceslav <v.j.vasiljev@gmail.com>
2024-07-25 18:26:04 +0300

    some more changes

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,27 @@ router.get("/new", (req, res) => {
   res.send("New user form");
 });
 
+router
+  .route("/:id")
+  .get((req, res) => {
+    req.params.id;
+    res.send(`Get User With ID ${req.params.id}`);
+    // console.log(req.user);
+  })
+  .put((req, res) => {
+    req.params.id;
+    res.send(`Update User With ID ${req.params.id}`);
+  })
+  .delete((req, res) => {
+    req.params.id;
+    res.send(`Delete User With ID ${req.params.id}`);
+  });
+
+const users = [{ name: "John" }, { name: "Jane" }, { name: "Joe" }];
+router.param("id", (req, res, next, id) => {
+  console.log(`User ID is: ${id}`);
+  req.user = users[id];
+  next();
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const app = express();
 
-app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  //   res.status(500).json({ message: "Hello World!" });
-  res.render("index", { text: "World" });
-});
+app.set("view engine", "ejs");
+// app.use(logger);
+
+// app.get("/", logger, (req, res) => {
+//   //   res.status(500).json({ message: "Hello World!" });
+//   res.render("index", { text: "World" });
+// });
 
 const userRouter = require("./routes/users");
 
 app.use("/users", userRouter);
+
+function logger(req, res, next) {
+  console.log(req.originalUrl);
+  next();
+}
 
 app.listen(3000);
